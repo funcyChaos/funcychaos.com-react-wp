4b87eee2182914bf1d708845fc662e1c83baf729
Austin Reilly <areilly@bitwiseindustries.com>
2023-05-29 08:39:54 +0200

    refactor(base): change home load url
    Also add / to the posts fetch, not sure how it was working before lol

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -1,6 +1,6 @@
 export async function getAllPosts(){
 	try{
-		const response	= fetch('wp-json/wp/v2/posts')
+		const response	= fetch('/wp-json/wp/v2/posts?_embed&per_page=100')
 		const data 			= (await response).json()
 		const object 		= await data
 		return await object
